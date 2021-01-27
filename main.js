496b885f52a16872f86063ec9727d95fc88c385e
zulma <zumendez7@gmail.com>
2021-01-27 18:07:00 +0100

    exo terminé

---
+++ main.js
@@ -0,0 +1,29 @@
+// 1
+let redP = document.getElementsByClassName('redPurple');
+console.log(redP);
+
+// 2
+let redP2 = document.querySelectorAll('.redPurple');
+console.log(redP2);
+
+// 3
+let redP3 = document.querySelectorAll('.redPurple')[0];
+console.log(redP3);
+
+// 4
+let list = document.querySelectorAll('li');
+console.log(list);
+
+let para = document.querySelectorAll('p');
+console.log(para);
+
+let span = document.querySelectorAll('span');
+console.log(span);
+
+// 5
+let important = document.querySelectorAll('.important');
+console.log(important);
+
+// 6
+let last = document.querySelectorAll('.redPurple');
+console.log(last);
